cb55757abc26f856ea53a47325bad3ec96e631b3
Vivchik <vovka.azik@mail.ru>
2022-12-29 19:19:17 +0300

    Task_23

---
+++ Task_23/Program.cs
@@ -0,0 +1,34 @@
+﻿/* Напишите программу, которая принимает на вход число (N) 
+и выдаёт таблицу кубов чисел от 1 до N.*/
+
+Console.Clear();
+
+int number;
+while (true)
+{
+    Console.Write("Введите целое число: ");
+
+    try
+    {
+        number = Convert.ToInt32(Console.ReadLine()!);
+        break;
+    }
+    catch (Exception exc)
+    {
+        Console.WriteLine($"Вы ввели не число! {exc.Message}");
+    }
+}
+Console.Write($"{number} ---> ");
+
+int num_abs = Math.Abs(number);
+int count = 0;
+if (number == 0)
+{
+    Console.Write($"Отсутствуют числа от 1 до 0");
+    return;
+}
+while (count <= num_abs)
+{
+    Console.Write($"{Math.Pow(count, 3)}, ");
+    count++;
+}
